8f601f370f69b3ed5020d1af772b529eafc1a714
cinaaaa <sina.farhadi.cyber@gmail.com>
2023-09-02 20:48:12 +0330

    feat: closing button and refactored closing style

--- reado/src/content/index.ts
+++ reado/src/content/index.ts
@@ -9,26 +9,26 @@ chrome.runtime.onMessage.addListener((message: { selectedText: string }, _) => {
   }
 })
 
-// remove the elmenet when clicked outside
+// listen to click events on the body
 body.addEventListener('click', (e) => {
-  if (e.target === section) {
-    const section = e.target as HTMLDivElement
-    // check the clicked element class name
-    if (section.className !== 'text-selection-content') {
-      destroySection()
-    }
+  // check if the click is on the close button
+  if (e.target && (e.target as HTMLElement).classList.contains('close-button-content')) {
+    destroySection()
   }
 })
 
-// listen to the keydown event
-// and check that if the key is CTRL + M
 body.addEventListener('keydown', (e) => {
+  // and check that if the key is CTRL + M
   if (e.ctrlKey && e.key === 'm') {
     const selection = window.getSelection()
     const selectedText = selection?.toString().trim()
     if (selectedText && selectedText.length > 1) {
       buildSection(selectedText)
     }
+  }
+  // check if the key is ESC
+  if (e.key === 'Escape') {
+    destroySection()
   }
 })
 
@@ -41,6 +41,9 @@ function buildSection(text: string) {
 
   section.innerHTML = `
     <div class="text-selection-content">
+      <svg class="close-button-content" x="0px" y="0px" width="25" height="25" viewBox="0 0 50 50">
+      <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
+      </svg>
       <p>${text}</p>
     </div>
     `
