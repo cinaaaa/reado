9549fb1c81f532f62b160dc193630d54140159b1
cinaaaa <sina.farhadi.cyber@gmail.com>
2023-09-02 19:58:17 +0330

    feat: simple closing solution

--- reado/src/content/index.ts
+++ reado/src/content/index.ts
@@ -6,6 +6,9 @@ section.classList.add('text-section-parent')
 
 chrome.runtime.onMessage.addListener((message: { selectedText: string }, _) => {
   if (message.selectedText.length > 1) {
+    // disable the page scroll
+    body.style.overflow = 'hidden'
+
     body.appendChild(section)
     section.innerHTML = `
     <div class="text-selection-content">
@@ -15,5 +18,17 @@ chrome.runtime.onMessage.addListener((message: { selectedText: string }, _) => {
     return true
   } else {
     return false
+  }
+})
+
+// remove the elmenet when clicked outside
+body.addEventListener('click', (e) => {
+  if (e.target === section) {
+    const section = e.target as HTMLDivElement
+    // check the clicked element class name
+    if (section.className !== 'text-selection-content') {
+      body.removeChild(section)
+      body.style.overflow = 'auto'
+    }
   }
 })
