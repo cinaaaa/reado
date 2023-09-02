4af2f241b91a2afe3acae0cbc45e9a45469aabde
cinaaaa <sina.farhadi.cyber@gmail.com>
2023-09-02 21:01:47 +0330

    feat: max possible z-index value

--- reado/src/background/index.ts
+++ reado/src/background/index.ts
@@ -6,13 +6,10 @@ chrome.runtime.onInstalled.addListener(function () {
     contexts: ['selection'],
   })
 
-  // Open a new search tab when the user clicks a context menu
   chrome.contextMenus.onClicked.addListener(async (item, tab) => {
     chrome.tabs.sendMessage(tab?.id as number, { selectedText: item.selectionText as string })
   })
 
-  // Intentionally create an invalid item, to show off error checking in the
-  // create callback.
   chrome.contextMenus.create({ title: 'Oops', parentId: 999, id: 'errorItem' }, function () {
     if (chrome.runtime.lastError) {
       console.log('Got expected error: ' + chrome.runtime.lastError.message)
