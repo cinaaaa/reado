eabe4d39238a7bdab5e006b3b27ea17bec19ab5e
cinaaaa <sina.farhadi.cyber@gmail.com>
2023-09-08 16:13:48 +0330

    fix: ready to be published

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,23 +1,27 @@
-import { EventTypes } from '../constants/enums';
+import { EventTypes } from "../constants/enums";
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
-    title: 'Magnify with Reado 🔎',
-    id: 'selection',
-    contexts: ['selection'],
+    title: "Magnify with Reado 🔎",
+    id: "selection",
+    contexts: ["selection"],
   });
 
   chrome.contextMenus.onClicked.addListener(async (item, _) => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id as number, { selectedText: item.selectionText as string });
+      chrome.tabs.sendMessage(tabs[0].id as number, {
+        selectedText: item.selectionText as string,
+      });
     });
   });
 });
 
 chrome.runtime.onMessage.addListener(function (request, _) {
-  if (request.message === EventTypes.CLICKED_ON_CONTEXT_MENU) {
+  if (request.message == EventTypes.CLICKED_ON_CONTEXT_MENU) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id as number, { message: 'clicked_browser_action' });
+      chrome.tabs.sendMessage(tabs[0].id as number, {
+        message: EventTypes.CLICKED_ON_CONTEXT_MENU,
+      });
     });
   }
 });
